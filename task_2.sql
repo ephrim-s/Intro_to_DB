381d14642762bad35b490f01c3e65d0f0e4ea821
ephrim-s <ephrims5@gmail.com>
2025-02-01 17:51:27 +0000

    updated

--- task_2.sql
+++ task_2.sql
@@ -1,13 +1,13 @@
 
 USE alx_book_store;
 
-CREATE TABLE authors (
+CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
 
-CREATE TABLE books (
+CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -17,7 +17,7 @@ CREATE TABLE books (
 );
 
 
-CREATE TABLE customers (
+CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE customers (
 );
 
 
-CREATE TABLE orders (
+CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
@@ -33,7 +33,7 @@ CREATE TABLE orders (
 );
 
 
-CREATE TABLE order_details (
+CREATE TABLE Order_details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
